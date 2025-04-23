05ad21a6308e54589067f46f119755d4ae414401
Kardosa Developer <dev@kardosa.com>
2025-04-22 21:47:04 -0400

    Disable CORS restrictions completely for production troubleshooting

--- backend/wsgi.py
+++ backend/wsgi.py
@@ -3,13 +3,17 @@ from flask_cors import CORS
 
 app = create_app()
 
-# Apply CORS directly to the application instance
-CORS(app, 
-     origins=['https://kardosa.xyz', 'https://www.kardosa.xyz'],
-     supports_credentials=True,
-     methods=['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-     allow_headers=['Content-Type', 'Authorization', 'X-Requested-With'],
-     expose_headers=['Content-Type', 'Authorization'])
+# Completely disable CORS restrictions - allow everything
+CORS(app, resources={r'/*': {'origins': '*'}}, supports_credentials=True)
+
+# Add middleware to force permissive headers on all responses
+@app.after_request
+def after_request(response):
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization,X-Requested-With')
+    response.headers.add('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS')
+    response.headers.add('Access-Control-Allow-Credentials', 'true')
+    return response
 
 if __name__ == '__main__':
     app.run()
